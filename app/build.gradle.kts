42116bbf5348e82b0777eb84746f817881830e7c
David <imanuel.david@student.umn.ac.id>
2025-11-21 22:46:05 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("commons-io:commons-io:2.6")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
